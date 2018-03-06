cb17a84c87067163fa34a303ad9b15d7bff0bdd1
D.Edwards <dae3f@uvawise.edu>
2018-03-06 01:19:27 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'pa87u6i4IghJ29s0jYM14cqJ8',
+  consumer_secret: '5bPMFt0DL6hEUeyPIMnhWi3WjgZUz4t5Q91MXeQQhCsGMTudaB',
+  access_token: '970897075335565312-XO8zDiS9BvFVZR0mbANZasl8FbqidZn',
+  access_token_secret: 'u51yeMM5iYXJHWdKXEAhnYIUBZ2EnsUqBnlQw3Mr8J0p5'
+}
